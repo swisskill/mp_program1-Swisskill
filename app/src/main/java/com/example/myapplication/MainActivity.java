bc339dbcbb716b07b4cf02ce4132dcb02f2eac18
swisskill <wbrant@uwyo.edu>
2022-09-07 14:58:01 -0600

    push for frags

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
+import android.widget.TextView;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager;
+
+        fragmentManager = getSupportFragmentManager();
+        //setup the mainFragment to show.
+        fragmentManager.beginTransaction().add(R.id.container, new helloFrag()).commit();
+//        TextView myView;
+//        myView = findViewById(R.id.textview);
+//        myView.setText("hello ere?");
     }
 }
+
+//to get text entry and a button:
+//find button is important
+//for text entry, you want no text in a text box, but a hint giving, well, a hint
+
+//how to do a view model
+//how to do an on click listener (toast?)
---
+++ app/src/main/java/com/example/myapplication/helloFrag.java
@@ -0,0 +1,36 @@
+package com.example.myapplication;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class helloFrag extends Fragment {
+
+    private HelloViewModel mViewModel;
+
+    public static helloFrag newInstance() {
+        return new helloFrag();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_hello, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(HelloViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
